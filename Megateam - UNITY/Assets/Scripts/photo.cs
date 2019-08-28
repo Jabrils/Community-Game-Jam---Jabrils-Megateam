b4e8aa69e1081517fb53957a5d9679f866e752b2
PPreda <fastgamingyt.mc444@gmail.com>
2019-08-28 15:31:11 +0300

    Now you can create images with F10
    Images are stored something like this C:/Users/PPreda/AppData/LocalLow/DefaultCompany/Megateam - UNITY/photos/photo_0.jpg
    As you create more the number in the file name goes up

--- Megateam - UNITY/Assets/Scripts/photo.cs
+++ Megateam - UNITY/Assets/Scripts/photo.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class photo : MonoBehaviour
 {
+    public Camera mainCam;
+    public List<string> photosCreated;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +17,39 @@ public class photo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.F10))
         {
-            ScreenCapture.CaptureScreenshot($"TheOne{Random.value}.png");
+            StartCoroutine(createPhoto());
         }
+    }
+
+    IEnumerator createPhoto()
+    {
+        Directory.CreateDirectory(Application.persistentDataPath + "/photos");
+        yield return new WaitForEndOfFrame();
+        mainCam.targetTexture = RenderTexture.GetTemporary(Screen.width, Screen.height, 16);
+
+        RenderTexture renderTexture = mainCam.targetTexture;
+        Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+        renderResult.ReadPixels(rect, 0, 0);
+
+        byte[] byteArray = renderResult.EncodeToJPG(50);
+
+        int Index = 0;
+
+        while(File.Exists(Application.persistentDataPath + "/photos/" + "photo_" + Index + ".jpg"))
+        {
+            Index++;
+        }
+
+        string finalPath = Application.persistentDataPath + "/photos/" + "photo_" + Index + ".jpg";
+
+        photosCreated.Add(finalPath);
+        File.WriteAllBytes(finalPath, byteArray);
+        Debug.Log("Image Created At: " + finalPath);
+
+        RenderTexture.ReleaseTemporary(renderTexture);
+        mainCam.targetTexture = null;
     }
 }
