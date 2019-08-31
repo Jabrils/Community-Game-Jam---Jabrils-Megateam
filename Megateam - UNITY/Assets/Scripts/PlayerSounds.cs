772638d5ac686ca1f163b581bf0f43663484581e
PPreda <fastgamingyt.mc444@gmail.com>
2019-08-31 03:38:01 +0300

    Tried adding step sounds
    The step sounds I could create here at 3:30am sounds more like wet towels than grass and the ones that I could record before were ruined by crickets. Forgive me lord Jabrils

---
+++ Megateam - UNITY/Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    public AudioClip[] GrassStepSounds;
+    public AudioClip[] ConcreteStepSounds;
+    public AudioClip[] GravelStepSounds;
+    public Rigidbody PlayerRb;
+    public stepSound soundType;
+
+    private bool soundLoop;
+
+    private void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal")).normalized;
+        if (move.x > 0 || move.y > 0)
+        {
+            if(!soundLoop)
+            {
+                StartCoroutine(stepSoundLoop());
+            }
+        }
+    }
+
+    IEnumerator stepSoundLoop()
+    {
+        soundLoop = true;
+        yield return new WaitForSeconds(Input.GetButton("Fire3") ? 0.25f : 0.30f);
+        switch(soundType)
+        {
+            case stepSound.Grass:
+                SoundManager.instance.PlaySound(GrassStepSounds[Random.Range(0, GrassStepSounds.Length)], PlayerRb.transform.position, 0.01f, Random.Range(0.9f, 1.1f), true);
+                break;
+            case stepSound.Concrete:
+                SoundManager.instance.PlaySound(ConcreteStepSounds[Random.Range(0, ConcreteStepSounds.Length)], PlayerRb.transform.position, 0.01f, Random.Range(0.9f, 1.1f), true);
+                break;
+            case stepSound.Gravel:
+                SoundManager.instance.PlaySound(GravelStepSounds[Random.Range(0, GravelStepSounds.Length)], PlayerRb.transform.position, 0.01f, Random.Range(0.9f, 1.1f), true);
+                break;
+        }
+        soundLoop = false;
+    }
+
+    public enum stepSound { Grass, Concrete, Gravel }
+}
---
+++ Megateam - UNITY/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    public uint sourcesForPooling;
+
+    public List<AudioSource> freeSources;
+    public List<AudioSource> busySources;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < sourcesForPooling; i++)
+        {
+            GameObject temp = new GameObject("Sound");
+            temp.transform.SetParent(transform);
+            freeSources.Add(temp.AddComponent<AudioSource>());
+            temp.SetActive(false);
+        }
+    }
+
+    public void PlaySound(AudioClip clip, Vector3 position, float volume = 1, float pitch = 1, bool is3d = false)
+    {
+        AudioSource temp = freeSources[0];
+        temp.transform.position = position;
+        temp.pitch = pitch;
+        if(is3d)
+        {
+            temp.spatialBlend = 1;
+            temp.minDistance = 5;
+            temp.maxDistance = 7.5f;
+        }
+        StartCoroutine(AddToBusySources(temp, clip.length));
+        temp.PlayOneShot(clip, volume);
+    }
+
+    public IEnumerator AddToBusySources(AudioSource audioSource, float time)
+    {
+        audioSource.gameObject.SetActive(true);
+        freeSources.Remove(audioSource);
+        busySources.Add(audioSource);
+        yield return new WaitForSeconds(time);
+        busySources.Remove(audioSource);
+        freeSources.Add(audioSource);
+        audioSource.gameObject.SetActive(false);
+    }
+}
